10f413402a09aaf24e7196d256ddbbc3f8695240
clementB94 <81488993+clementB94@users.noreply.github.com>
2022-11-15 18:32:26 +0100

    html render

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from bs4 import BeautifulSoup
 import requests
 import re
@@ -6,52 +9,22 @@ import pandas as pd
 import psycopg2
 import numpy as np
 import psycopg2.extras as extras
-#from insert_db import insert_db
 from app.database import Database
 
 app = FastAPI(title="API WebApp")
+templates = Jinja2Templates(directory="frontend/")
+
 db = Database()
 
 
-
-    
-
-def execute_values(conn, df, table):
-    
-    tuples = [tuple(x) for x in df.to_numpy()]
-
-    cols = ','.join(list(df.columns))
-    query = "INSERT INTO %s(%s) VALUES %%s" % (table, cols)
-    cursor = conn.cursor()
-    try:
-        extras.execute_values(cursor, query, tuples)
-        conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print("Error: %s" % error)
-        conn.rollback()
-        cursor.close()
-        return 1
-    print("the dataframe is inserted")
-    cursor.close()
-
-
-
 @app.get('/')
-def Home():
+def Home(request: Request):
 
-    # conn = psycopg2.connect(database="postgres", user="postgres", password="postgres", host="database", port="5432")
-    # cursor = conn.cursor()
-    # postgreSQL_select_Query = "select * from IMDB"
-    # cursor.execute(postgreSQL_select_Query)
-    # imdb = cursor.fetchall()
     imdb = db.get_movies()
     
-    return {row for row in imdb}
+    return templates.TemplateResponse('home.html', context={'request': request, 'imdb': imdb})
 
 @app.get('/test')
 def test():
 
     return db.test()
-
-
-# insert_db()
